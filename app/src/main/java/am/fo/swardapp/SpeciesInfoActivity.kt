2d28637cbd83c6feb8a6d106448441ec8e525775
dave griffiths <dave@fo.am>
2020-08-06 10:34:47 +0100

    ID flow working for legumes, new beta release

---
+++ app/src/main/java/am/fo/swardapp/SpeciesInfoActivity.kt
@@ -0,0 +1,11 @@
+package am.fo.swardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeciesInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_species_info)
+    }
+}
--- app/src/main/java/am/fo/swardapp/id_fragments/IdResultFragment.kt
+++ app/src/main/java/am/fo/swardapp/id_fragments/IdResultFragment.kt
@@ -1,18 +1,21 @@
 package am.fo.swardapp.id_fragments
 
+import am.fo.swardapp.IdentificationActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import am.fo.swardapp.R
+import am.fo.swardapp.SpeciesActivity
+import android.content.Intent
 import kotlinx.android.synthetic.main.fragment_id_result.*
 import kotlinx.android.synthetic.main.fragment_id_result.view.*
 
 class IdResultFragment : Fragment() {
     private var answer: String? = null
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private val ARG_ANSWER = "answer"
+    class Desc(val name: Int, val lat: Int, val img: Int )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +29,43 @@ class IdResultFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view:View = inflater.inflate(R.layout.fragment_id_result, container, false)
+        return inflater.inflate(R.layout.fragment_id_result, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         answer?.let {
-            view.some_text.setText(it)
+            val desc: Desc = when (answer) {
+                    "grass_cocksfoot" -> Desc(R.string.grass_cocksfoot,R.string.grass_cocksfoot_lat,R.drawable.grass_cocksfoot_plant)
+                    "grass_meadowfescue" -> Desc(R.string.grass_meadowfescue,R.string.grass_meadowfescue_lat,R.drawable.grass_meadowfescue_plant)
+                    "grass_meadowfoxtail" -> Desc(R.string.grass_meadowfescue,R.string.grass_meadowfescue_lat,R.drawable.grass_meadowfescue_plant)
+                    "grass_perennialryegrass" -> Desc(R.string.grass_perennialryegrass,R.string.grass_perennialryegrass_lat,R.drawable.grass_perennialryegrass_plant)
+                    "grass_tallfescue" -> Desc(R.string.grass_tallfescue,R.string.grass_tallfescue_lat,R.drawable.grass_tallfescue_plant)
+                    "grass_timothy" -> Desc(R.string.grass_timothy,R.string.grass_timothy_lat,R.drawable.grass_timothy_plant)
+                    "legume_alsike" -> Desc(R.string.legume_alsike,R.string.legume_alsike_lat,R.drawable.legume_alsike_leaf)
+                    "legume_birdsfootrefoil" -> Desc(R.string.legume_birdsfootrefoil,R.string.legume_birdsfootrefoil_lat,R.drawable.legume_birdsfoottrefoil_plant)
+                    "legume_lucern" -> Desc(R.string.legume_lucern,R.string.legume_lucern_lat,R.drawable.legume_lucerne_plant)
+                    "legume_redclover" -> Desc(R.string.legume_redclover,R.string.legume_redclover_lat,R.drawable.legume_redclover_leaf_1)
+                    "legume_sainfoin" -> Desc(R.string.legume_sainfoin,R.string.legume_sainfoin_lat,R.drawable.legume_sainfoin_leaf)
+                    "legume_whiteclover" -> Desc(R.string.legume_whiteclover,R.string.legume_whiteclover_lat,R.drawable.legume_whiteclover_leaf)
+                    else -> Desc(R.string.legume_whiteclover,R.string.legume_whiteclover_lat,R.drawable.legume_whiteclover_leaf)
+            }
+
+            species_name.setText(desc.name)
+            species_name_lat.setText(desc.lat)
+            species_photo.setImageResource(desc.img)
         }
 
-        return view
+        species_info.setOnClickListener {}
+
+        id_again.setOnClickListener {
+            startActivity(Intent(getActivity(),IdentificationActivity::class.java))
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(answer: String) =
             IdResultFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_ANSWER, answer)
